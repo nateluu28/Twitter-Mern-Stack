717037ce82e5d86d707687cce8ab382b174c654e
Nathan Luu <nateluu28@gmail.com>
2021-06-13 16:30:28 -0700

    Added tweet validations

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const passport = require('passport');
+require('./config/passport')(passport);
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -20,14 +22,6 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
 
-app.get('/', (req,res) =>{
-  const user = new User ({
-    handle: 'jim',
-    email: 'adlkjf',
-    password: '123123'
-  })
-  user.save()
-  res.send('Hello world')
-})
+app.use(passport.initialize());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/Users');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get('/test', (req,res) => {
   res.json({msg: 'This is the users route'}) 
@@ -10,6 +14,11 @@ router.get('/test', (req,res) => {
 
 router.post('/register', (req,res) => {
   //Check if dup emails
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({email: req.body.email})
     .then(user => {
       if(user){
@@ -36,22 +45,40 @@ router.post('/register', (req,res) => {
 })
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({email})
     .then(user => {
       if (!user) {
-        return res.status(404).json({email: 'This user does not exist'});
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({msg: 'Success'});
+            const payload = { id: user.id, handle: user.handle };jwt.sign(
+            payload,
+            keys.secretOrKey,
+            // Tell the key to expire in one hour
+            {expiresIn: 3600},
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            });
           } else {
-            return res.status(400).json({password: 'Incorrect password'});
-          }
+              errors.password = 'Incorrect password'
+              return res.status(400).json(errors);          
+            }
         })
     })
 })
