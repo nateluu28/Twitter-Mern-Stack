3d7294237d71570c7d1f92189e8afba4b8ab0a72
Nathan Luu <nateluu28@gmail.com>
2021-06-13 10:29:12 -0700

    Added login and register routes

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const mongoose = require('mongoose');
-const db = require('./config/keys');
-
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('Connected to MongoDB successfully'))
-  .catch(err => console.log(err));
+const db = require('./config/keys').mongoURI
+const bodyParser = require('body-parser');
 
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.get('/', (req,res) => res.send('Hello world'));
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose
+  .connect(db, { useNewUrlParser: true,  useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+
+app.get('/', (req,res) =>{
+  const user = new User ({
+    handle: 'jim',
+    email: 'adlkjf',
+    password: '123123'
+  })
+  user.save()
+  res.send('Hello world')
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
